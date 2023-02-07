6f091fe32141bbc47349263fa2871e93127ace1e
Kalande_Kapaya <kjkreed@gmail.com>
2023-02-07 23:58:59 +0200

    query selector  2nd fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const main = document.querySelector('main')
+const main = document.querySelector('.main')
 
 open.addEventListener('click', () => main.classList.add('show-nav'))
 close.addEventListener('click', () => main.classList.remove('show-nav'))
